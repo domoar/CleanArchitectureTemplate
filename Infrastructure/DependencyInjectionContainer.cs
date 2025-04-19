94ae0dc632b0cf83cb6367de17576031ce6b3d95
ManuelDausmann <156981587+domoar@users.noreply.github.com>
2025-04-19 19:33:16 +0200

    added default repository

--- Infrastructure/DependencyInjectionContainer.cs
+++ Infrastructure/DependencyInjectionContainer.cs
@@ -9,30 +9,7 @@ using Microsoft.Extensions.Logging;
 namespace Infrastructure;
 
 public static class DependencyInjectionContainer {
-
-  const int POOL_SIZE = 1024;
-
   public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
-    //services.AddSingleton<HealthCheckResilienceService>();
-
-    services.AddPooledDbContextFactory<DefaultContext>((sp, options) => {
-      options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-        options.EnableSensitiveDataLogging(true);
-
-      var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
-      options.UseLoggerFactory(loggerFactory);
-      options.LogTo(
-        message => loggerFactory.CreateLogger("EFCore"),
-        [DbLoggerCategory.Database.Command.Name],
-        LogLevel.Information,
-        DbContextLoggerOptions.SingleLine
-      );
-
-      //options.UseSqlServer(...)
-    },
-    POOL_SIZE
-    );
-
     return services;
   }
 }
