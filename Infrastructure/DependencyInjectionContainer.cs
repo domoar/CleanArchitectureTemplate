6270a2fcdeba963455334cb4439f3289f795b7cf
ManuelDausmann <156981587+domoar@users.noreply.github.com>
2025-04-19 18:23:47 +0200

    added default ctx for demonstration purposes

--- Infrastructure/DependencyInjectionContainer.cs
+++ Infrastructure/DependencyInjectionContainer.cs
@@ -1,11 +1,38 @@
 ﻿using Infrastructure.services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Infrastructure.context;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure;
 
 public static class DependencyInjectionContainer {
+
+  const int POOL_SIZE = 1024;
+
   public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
     //services.AddSingleton<HealthCheckResilienceService>();
+
+    services.AddPooledDbContextFactory<DefaultContext>((sp, options) => {
+      options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        options.EnableSensitiveDataLogging(true);
+
+      var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+      options.UseLoggerFactory(loggerFactory);
+      options.LogTo(
+        message => loggerFactory.CreateLogger("EFCore"),
+        [DbLoggerCategory.Database.Command.Name],
+        LogLevel.Information,
+        DbContextLoggerOptions.SingleLine
+      );
+
+      //options.UseSqlServer(...)
+    },
+    POOL_SIZE
+    );
+
     return services;
   }
 }
---
+++ Infrastructure/context/DefaultContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.context;
+public class DefaultContext : DbContext {
+  private readonly ILogger<DefaultContext> _logger;
+
+  public DefaultContext(ILoggerFactory loggerFactory, DbContextOptions<DefaultContext> options) : base(options) {
+    _logger = loggerFactory.CreateLogger<DefaultContext>();
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    base.OnModelCreating(modelBuilder);
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+    base.OnConfiguring(optionsBuilder);
+  }
+
+  private static void ConfigureDevelopment(ModelBuilder modelBuilder) {
+  }
+
+  private static void ConfigureStaging(ModelBuilder modelBuilder) {
+  }
+
+  private static void ConfigureProduction(ModelBuilder modelBuilder) {
+  }
+}
